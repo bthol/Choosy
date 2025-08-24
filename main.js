40ae378c6595b5259ca4764c5400082e7b88d4c9
bthol <bthollaug@gmail.com>
2025-08-24 09:14:57 -0700

    conditional functionality

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 const optionsContainer = document.querySelector('.options-container');
+const selectedDisplay = document.querySelector('#selected-option-display');
+const selectionMethod = document.querySelector('#selection-method');
 let options = [];
 let optionsIndex = 0;
 function selectOption() {
@@ -13,19 +15,20 @@ function selectOption() {
                 copy.splice(index, 1);
             }
         }
-        document.querySelector('#selected-option-display').innerHTML = options[optionsIndex];
+        selectedDisplay.innerHTML = options[optionsIndex];
         optionsIndex += 1;
         if (optionsIndex === options.length) {
             optionsIndex = 0;
         }
         
     } else if (method === 'random-option') {
-        document.querySelector('#selected-option-display').innerHTML = options[Math.floor(Math.random() * options.length)];
+        selectedDisplay.innerHTML = options[Math.floor(Math.random() * options.length)];
     }
 };
 function clearOptions() {
     optionsContainer.innerHTML = '';
     options = [];
+    selectedDisplay.innerHTML = '';
 };
 function removeOption(event) {
     event.target.removeEventListener('click', removeOption);
@@ -39,26 +42,40 @@ function removeOption(event) {
     event.target.parentNode.remove();
 };
 function addOption() {
-    options.push(document.querySelector('#option-field').value);
-    let optionsHTML = '<ul>';
-    for (let i = 0; i < options.length; i++) {
-        optionsHTML += `<div class="list-option-style"><li>${options[i]}</li><button class="remove-option-btn">remove</button></div>`;
+    const option = document.querySelector('#option-field').value;
+    if (option !== '') {
+        options.push(option);
+        let optionsHTML = '<ul>';
+        for (let i = 0; i < options.length; i++) {
+            optionsHTML += `<div class="list-option-style"><li>${options[i]}</li><button class="remove-option-btn">remove</button></div>`;
+        }
+        optionsHTML += '</ul>';
+        optionsContainer.innerHTML = optionsHTML;
+        document.querySelectorAll('.remove-option-btn').forEach((btn) => {
+            btn.addEventListener('click', removeOption);
+        });
+        if (selectionMethod.value === 'random-order') {
+            let copy = options;
+            options = [];
+            while (copy.length > 0) {
+                const index = Math.floor(Math.random() * copy.length); 
+                options.push(copy[index]);
+                copy.splice(index, 1);
+            }
+        }
     }
-    optionsHTML += '</ul>';
-    optionsContainer.innerHTML = optionsHTML;
-    document.querySelectorAll('.remove-option-btn').forEach((btn) => {
-        btn.addEventListener('click', removeOption);
-    });
 };
 document.querySelector('#select-option-btn').addEventListener('click', selectOption);
 document.querySelector('#add-option-btn').addEventListener('click', addOption);
 document.querySelector('#clear-options-btn').addEventListener('click', clearOptions);
-document.querySelector('#selection-method').addEventListener('change', () => {
-    let copy = options;
-    options = [];
-    while (copy.length > 0) {
-        const index = Math.floor(Math.random() * copy.length); 
-        options.push(copy[index]);
-        copy.splice(index, 1);
+selectionMethod.addEventListener('change', () => {
+    if (selectionMethod.value === 'random-order') {
+        let copy = options;
+        options = [];
+        while (copy.length > 0) {
+            const index = Math.floor(Math.random() * copy.length); 
+            options.push(copy[index]);
+            copy.splice(index, 1);
+        }
     }
 });
